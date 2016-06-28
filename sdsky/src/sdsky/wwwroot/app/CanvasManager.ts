343aa169d4ade9ec84bf0275bc986f4423c3dafd
sdcb <flysha@live.com>
2016-06-28 17:48:13 +0800

    setup engine for sdsky

---
+++ sdsky/src/sdsky/wwwroot/app/CanvasManager.ts
@@ -0,0 +1,87 @@
+﻿namespace sdsky {
+    type IBrushType = string | CanvasGradient | CanvasPattern;
+
+    export class CanvasManager {
+        private ctx: CanvasRenderingContext2D;
+
+        constructor(private canvas: HTMLCanvasElement) {
+            this.ctx = canvas.getContext("2d");
+        }
+
+        get width() {
+            return this.canvas.width;
+        }
+
+        get height() {
+            return this.canvas.height;
+        }
+
+        fillRect(x: number, y: number, w: number, h: number, brush: IBrushType) {
+            this.ctx.fillStyle = brush;
+            this.ctx.fillRect(x, y, w, h);
+        }
+
+        strokeRect(x: number, y: number, w: number, h: number, brush: IBrushType, lineWidth?: number) {
+            if (lineWidth) this.ctx.lineWidth = lineWidth;
+            this.ctx.strokeStyle = brush;
+            this.ctx.strokeRect(x, y, w, h);
+        }
+
+        fillText(text: string, x: number, y: number, brush: IBrushType, maxWidth?: number): void {
+            this.ctx.fillStyle = brush;
+            maxWidth && this.ctx.fillText(text, x, y, maxWidth);
+            maxWidth || this.ctx.fillText(text, x, y);
+        }
+
+        strokeText(text: string, x: number, y: number, brush: IBrushType, maxWidth?: number): void {
+            this.ctx.strokeStyle = brush;
+            maxWidth && this.ctx.strokeText(text, x, y, maxWidth);
+            maxWidth || this.ctx.strokeText(text, x, y);
+        }
+
+        line(p1x: number, p1y: number, p2x: number, p2y: number) {
+            this.ctx.moveTo(p1x, p1y);
+            this.ctx.lineTo(p2x, p2y);
+        }
+
+        drawLine(p1x: number, p1y: number, p2x: number, p2y: number, brush?: IBrushType, lineWidth?: number) {
+            if (lineWidth) this.ctx.lineWidth = lineWidth;
+            if (brush) this.ctx.strokeStyle = brush;
+            this.ctx.beginPath();
+            this.ctx.moveTo(p1x, p1y);
+            this.ctx.lineTo(p2x, p2y);
+            this.ctx.stroke();
+        }
+
+        fillCircle(x: number, y: number, r: number, brush?: IBrushType) {
+            if (brush) this.ctx.fillStyle = brush;
+            this.ctx.beginPath();
+            this.ctx.moveTo(x, 0);
+            this.ctx.arc(x, y, r, 0, Math.PI * 2);
+            this.ctx.fill();
+        }
+
+        circle(x: number, y: number, r: number) {
+            this.ctx.moveTo(x, 0);
+            this.ctx.arc(x, y, r, 0, Math.PI * 2);
+        }
+
+        rotate(angle: number, centerX?: number, centerY?: number) {
+            this.ctx.rotate(angle % 360);
+            centerX && this.ctx.translate(-centerX, -centerY);
+        }
+
+        setIdentity() {
+            this.ctx.setTransform(1, 0, 0, 1, 0, 0);
+        }
+
+        clear() {
+            this.setIdentity();
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        }
+
+        transform(matrix: float3x2) {
+            this.ctx.transform.apply(this.ctx, matrix.get());
+        }
+    }
+}
---
+++ sdsky/src/sdsky/wwwroot/app/PromiseEvent.ts
@@ -0,0 +1,40 @@
+﻿namespace sdsky {
+    type IPromiseEventCallback<T> = (value: T) => JQueryPromise<any> | any;
+    type IPromiseEventCallbackVoid = () => JQueryPromise<any> | any;
+
+    abstract class PromiseEventBase<ActionType> {
+        _actions = Array<ActionType>();
+
+        connect(action: ActionType) {
+            this._actions.push(action);
+        }
+
+        disconnect(action: ActionType) {
+            this._actions.splice(this._actions.indexOf(action), 1);
+        }
+    }
+
+    export class PromiseEvent<T> extends PromiseEventBase<IPromiseEventCallback<T>> {
+        fire(value: T) {
+            let defer = $.Deferred<void>();
+            $.when(...this._actions.map(a => a(value))).then(() => {
+                defer.resolve();
+            }).fail(() => {
+                defer.reject();
+            });
+            return defer.promise();
+        }
+    }
+
+    export class PromiseEventVoid extends PromiseEventBase<IPromiseEventCallbackVoid> {
+        fire() {
+            let defer = $.Deferred<void>();
+            $.when(...this._actions.map(a => a(void 0))).then(() => {
+                defer.resolve();
+            }).fail(() => {
+                defer.reject();
+            });
+            return defer.promise();
+        }
+    }
+}
